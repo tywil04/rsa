1be6590e89a716c5471e700cc4e251e206d67bdf
Tyler <68185249+tywil04@users.noreply.github.com>
2022-10-12 14:08:06 +0000

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -6,9 +6,7 @@ class rsa:
     abc = list(string.ascii_letters + string.digits + string.punctuation + " "); # List of all lowercase letters, uppercase letters, numbers and punctuation plus a whitespace
 
     # Generates a public and private key pair
-    def generateKeyPair() -> tuple:
-        p = 11; # Prime 1
-        q = 13; # Prime 2
+    def generateKeyPair(p, q):
         n = p * q;
         phi = (p - 1)*(q - 1) ;
         e = rsa.generateE(phi);
@@ -17,7 +15,7 @@ class rsa:
         return ((e, n), (d, n)); # Returns public key (e,n) and private key (d,n) as tuple;
 
     # Extended Euclidean algorithm - This was taken from https://www.tutorialspoint.com/python-program-for-extended-euclidean-algorithms and tweaked to suit my needs
-    def egcd(e: int, phi: int) -> int:
+    def egcd(e: int, phi: int):
         d = 0;
         x1 = 0;
         x2 = 1;
@@ -42,14 +40,14 @@ class rsa:
             return d + phi;
 
     # Generates e. E is a coprime of phi because the only factor in common is 1.
-    def generateE(phi: int) -> int:
+    def generateE(phi: int):
         e = random.SystemRandom().randrange(1, phi); # Generate test e (less than phi)
         if math.gcd(e, phi) != 1: # If the test e is not a coprime of phi
             return rsa.generateE(phi); # Recursively call this function until e is a coprime of phi
         return e;
 
     # Encrypt using a generate public key and a userdefined plainText
-    def encrypt(publicKey: int, plainText: str) -> None:
+    def encrypt(publicKey: int, plainText: str):
         # Iterates through plainText
         # Turns each character into a number using abc (an array of characters and numbers)
         # Runs the (p^e)%n using provided public key
@@ -58,7 +56,7 @@ class rsa:
         return bytes([(rsa.abc.index(char)**publicKey[0] % publicKey[1]) for char in plainText]);
 
     # Encrypt using a generate private key and a userdefined cipherText
-    def decrypt(privateKey: int, cipherText: str) -> None:
+    def decrypt(privateKey: int, cipherText: str):
         # Iterates through cipherText
         # Turns each character into a number using abc (an array of characters and numbers)
         # Runs the (c^e)%n using provided private key where c is the ciphertext
@@ -66,7 +64,9 @@ class rsa:
 
         return "".join([rsa.abc[(char**privateKey[0]) % privateKey[1]] for char in cipherText]);
 
-publicKey, privateKey = rsa.generateKeyPair(); # Generate keypair
+p = 11; # Prime 1
+q = 13; # Prime 2
+publicKey, privateKey = rsa.generateKeyPair(p, q); # Generate keypair
 
 encrypted = rsa.encrypt(publicKey,"This is a test!");
 decrypted = rsa.decrypt(privateKey, encrypted);
